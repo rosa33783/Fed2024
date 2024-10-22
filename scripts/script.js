17e87037cba7bfddaffa3e7a5719128e879495bd
rosa33783 <112877989+rosa33783@users.noreply.github.com>
2024-10-22 19:41:11 +0200

    Add files via upload
    styling index 80%
    nog geen styling details

---
+++ scripts/script.js
@@ -0,0 +1,59 @@
+/*code van de gemaakte hamburgermenu opdracht codepen*/
+
+/******************************/
+/* menu openen de MENU button */
+/******************************/
+
+const menuOpenButton = document.querySelector("header button:nth-of-type(1)");
+const deNav = document.querySelector("header nav");
+
+menuOpenButton.onclick = openMenu;
+
+function openMenu(){
+  deNav.classList.add("toonMenu")
+}
+
+
+
+
+/************************************/
+/* menu sluiten met de sluit button */
+/************************************/
+
+const menuSluitButton = document.querySelector("nav button");
+
+menuSluitButton.onclick = sluitMenu;
+
+function sluitMenu(){
+  deNav.classList.remove("toonMenu");
+}
+
+
+
+
+
+
+
+const prevButton = document.querySelector(" main button:nth-of-type(1)");
+const nextButton = document.querySelector(" main button:nth-of-type(2)");
+const deUl = document.querySelector("main ul");
+
+
+function goToNextPhoto() {
+	const deULWidth = deUl.offsetWidth;
+	const deULWidthHalf = deULWidth * .5;
+	deUl.scrollLeft = deUl.scrollLeft + deULWidthHalf;
+}
+
+
+nextButton.onclick = goToNextPhoto;
+
+
+function goToPrevPhoto() {
+	const deULWidth = deUl.offsetWidth;
+	const deULWidthHalf = deULWidth * .5;
+	deUl.scrollLeft = deUl.scrollLeft - deULWidthHalf;
+}
+
+
+prevButton.onclick = goToPrevPhoto;
